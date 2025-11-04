d431c682d9e1e24e25fc2f25436d51f4c844a54a
Muhamad Akbar Muzakynur <148756201+MuzakyAkbar@users.noreply.github.com>
2025-11-04 14:10:27 +0700

    Add files via upload

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="header">
+    <div class="header-logo">J</div>
+    <div class="header-menu">
+      <div class="menu-line"></div>
+      <div class="menu-line"></div>
+      <div class="menu-line"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
